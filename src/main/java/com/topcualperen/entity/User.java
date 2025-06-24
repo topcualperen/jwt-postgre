f8897451c2edad93ba09790911f09e477dd753ca
topcualperen <alperenttpc78@gmail.com>
2025-06-24 22:59:42 +0300

    added schema

--- src/main/java/com/topcualperen/entity/User.java
+++ src/main/java/com/topcualperen/entity/User.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 @Entity
-@Table(name = "users")
+@Table(name = "users", schema = "jwttt")
 public class User {
 
     @Id
